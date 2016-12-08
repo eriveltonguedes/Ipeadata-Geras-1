fbdb7c04bc6df1a7732d385b7b791f3ca2341fbf
Luiz Eduardo Gomes <leduardo.gomes@hotmail.com>
2016-12-08 16:55:30 -0200

    Alteração 08/12/2016
    1) Alterar para mostrar os 5 ultimos

--- GM366ger.R
+++ GM366ger.R
@@ -27,9 +27,11 @@ for (i in 1:length(pacotes))
   if (length(names(installed.packages()[,1])[names(installed.packages()[,1])==pacotes[i]])==0){install.packages(pacotes[i], repos="http://cran.fiocruz.br/")}
   library(pacotes[i],character.only = TRUE) 
 }
+
 ## 1) AJUSTES DOS PARAMETROS
 options(scipen=999)
 setwd("//Srjn3/Area_Corporativa/Projeto_IPEADATA/Temporario/geras")
+
 ## 2) CARREGANDO AS SERIES INPUT E DEFININDO AS SERIES OUTPUT
 seroutput<-c("GM366_EREURO366")
 serinput<-c("GM366_ERV366",
@@ -40,22 +42,27 @@ for(i in 1:length(serinput))
   assign(nomes,sqlQuery((odbcConnect("ipeadata",uid="",pwd="")),(paste0("SELECT ipea.vw_Valor.SERCODIGO, CAST (ipea.vw_Valor.VALDATA as NUMERIC) as VALDATA, ipea.vw_Valor.VALVALOR FROM ipea.vw_Valor WHERE (((ipea.vw_Valor.SERCODIGO)='",serinput[i],"' and ipea.vw_Valor.VALVALOR IS NOT NULL)) order by VALDATA;"))))
   odbcCloseAll()
 }
+
 ## 3) FUNDINDO AS SERIES EM UM UNICO BLOCO DE DADOS
 serie<-merge(serie1,serie2,by="VALDATA",all=T)
 serie$VALDATA<-as.Date(serie$VALDATA, origin = "1900-01-01")
+
 ## 4) BLOCO DE OPERACOES MATEMATICAS
 GERADO<-data.frame(NULL)
 for (i in 1:(dim(serie)[1])){GERADO[i,1]<-serie[i,3]/serie[i,5]}
+
 ## 5) COLOCANDO NO FORMATO NOVO (PLANILHA GENERICA)
 GENERICA<-data.frame(serie[,1],GERADO)
 names(GENERICA)<-c("VALDATA",seroutput)
 r<-NULL
 for (i in 1:dim(GENERICA)[1]){if (sum(is.na(GENERICA[i,]))==0){r<-c(r,i)}}
-if (length(r)>0){GENERICA<-GENERICA[r[length(r)]:dim(GENERICA)[1],]}
+if (length(r)>0){GENERICA<-GENERICA[r[length(r)-4]:dim(GENERICA)[1],]}
 GENERICA <- na.exclude(GENERICA)
 GENERICA[,2] <- round(GENERICA[,2],5)
+
 ## 6) SALVANDO EM .XLS
 write.xlsx(GENERICA,paste0("GM366ger.xls"),sheetName="Generica",row.names=F,showNA=F)
+
 #######################
 # ## 7) VERIFICACAO COM A SERIE NO BANCO
 # for(i in 1:length(seroutput))
