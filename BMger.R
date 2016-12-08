5f0895bed4d2003fdd899d8ebc567811ed402070
Luiz Eduardo Gomes <leduardo.gomes@hotmail.com>
2016-12-08 16:52:48 -0200

    Alteração 08/12/2016
    1) Alterar para mostrar os 5 ultimos

--- BMger.R
+++ BMger.R
@@ -34,9 +34,11 @@ for (i in 1:length(pacotes))
   if (length(names(installed.packages()[,1])[names(installed.packages()[,1])==pacotes[i]])==0){install.packages(pacotes[i], repos="http://cran.fiocruz.br/")}
   library(pacotes[i],character.only = TRUE) 
 }
+
 ## 1) AJUSTES DOS PARAMETROS
 options(scipen=999)
 setwd("//Srjn3/Area_Corporativa/Projeto_IPEADATA/Temporario/geras")
+
 ## 2) CARREGANDO AS SERIES INPUT E DEFININDO AS SERIES OUTPUT
 seroutput<-c("BM_RES",
              "BM_LFTPP",
@@ -54,12 +56,14 @@ for(i in 1:length(serinput))
   assign(nomes,sqlQuery((odbcConnect("ipeadata",uid="",pwd="")),(paste0("SELECT ipea.vw_Valor.SERCODIGO, CAST (ipea.vw_Valor.VALDATA as NUMERIC) as VALDATA, ipea.vw_Valor.VALVALOR FROM ipea.vw_Valor WHERE (((ipea.vw_Valor.SERCODIGO)='",serinput[i],"' and ipea.vw_Valor.VALVALOR IS NOT NULL)) order by VALDATA;"))))
   odbcCloseAll()
 }
+
 ## 3) FUNDINDO AS SERIES EM UM UNICO BLOCO DE DADOS
 serie<-merge(serie1,serie2,by="VALDATA",all=T)
 serie<-merge(serie,serie3,by="VALDATA",all=T)
 serie<-merge(serie,serie4,by="VALDATA",all=T,suffixes = c(".z",".w"))
 serie<-merge(serie,serie5,by="VALDATA",all=T,suffixes = c(".a",".b"))
 serie$VALDATA<-as.Date(serie$VALDATA, origin = "1900-01-01")
+
 ## 4) CRIANDO VETOR DE DATAS
 ## APLICADO EM CONVERSOES DE SERIES MENSAIS PARA ANUAIS ##
 # "Se os dados iniciam apos o mes 01, nao fica caracterizado um ano completo." # 
@@ -71,6 +75,7 @@ if ((as.POSIXlt(serie[dim(serie)[1],1])$mon+1==12)) {h<-1}
 datas<-seq(as.Date(paste0((as.POSIXlt(serie[1,1])$year+1900+k),"-01-01")),
            as.Date(paste0((as.POSIXlt(serie[dim(serie)[1],1])$year+1900+h),"-01-01")),
            by='1 year')  
+
 ## 5) BLOCO DE OPERACOES MATEMATICAS
 GERADO<-data.frame(NULL)
 for (i in 2:length(datas))
@@ -81,14 +86,17 @@ for (i in 2:length(datas))
   GERADO[i-1,4]<-as.numeric(tail(subset(serie, serie$VALDATA >= datas[i-1] & serie$VALDATA < datas[i] ,select=VALVALOR.w), n = 1L))
   GERADO[i-1,5]<-as.numeric(tail(subset(serie, serie$VALDATA >= datas[i-1] & serie$VALDATA < datas[i] ,select=VALVALOR), n = 1L))  
 }
+
 ## 6) COLOCANDO NO FORMATO NOVO (PLANILHA GENERICA)
 GENERICA<-data.frame(datas[-length(datas)],GERADO)
 names(GENERICA)<-c("VALDATA",seroutput)
 r<-NULL
 for (i in 1:dim(GENERICA)[1]){if (sum(is.na(GENERICA[i,]))==0){r<-c(r,i)}}
-if (length(r)>0){GENERICA<-GENERICA[r[length(r)]:dim(GENERICA)[1],]}
+if (length(r)>0){GENERICA<-GENERICA[r[length(r)-4]:dim(GENERICA)[1],]}
+
 ## 7) SALVANDO EM .XLS
 write.xlsx(GENERICA,paste0("BMger.xls"),sheetName="Generica",row.names=F,showNA=F)  
+
 # #######################
 # ##8) VERIFICACAO COM A SERIE NO BANCO
 # for(i in 1:length(seroutput))
